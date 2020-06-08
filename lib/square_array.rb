cc046d43944827c445e64aaf0e8daa957cb7a561
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-08 20:07:53 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  i = 0
+  square_array = []
+  while i < array.length do
+    square_array << array[i].to_i ** 2
+    i += 1
+  end
+  return square_array
 end
